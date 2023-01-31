6eb0d927a57e41c970799081fdfb8941195d9a7d
Tobi <tobiraheem@gmail.com>
2023-01-31 18:16:02 +0000

    minor update

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //pip3 install flask && python3 server.py
-
 let who = ['my daughter','my dog', 'my alter-ego', 'my bird'];
 let action = ['ate', 'crushed', 'destroyed', 'damaged'];
 let what = ['my homework', 'the car', 'the cake', 'my phone'];
 let when = ['right now', 'before class', 'last week', 'many moons ago']
-
 
 function randomiseRefactor (x){
     return x[Math.floor(Math.random()*x.length)]
